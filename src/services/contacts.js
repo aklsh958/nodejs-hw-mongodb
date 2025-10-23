5c2d369440cc398a8ec55a7cca7856de6e5a05b2
Artem Kulish <akulish247@gmail.com>
2025-10-23 15:10:51 +0200

    update

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,21 +1,21 @@
-import { Contact } from '../models/contact.js';
+import { ContactsCollection } from '../models/contact.js';
 
 export const getAllContactsService = async (userId, skip, limit) => {
-  return await Contact.find({ userId }).skip(skip).limit(limit);
+  return await ContactsCollection.find({ userId }).skip(skip).limit(limit);
 };
 
 export const getContactByIdService = async (contactId, userId) => {
-  return await Contact.findOne({ _id: contactId, userId });
+  return await ContactsCollection.findOne({ _id: contactId, userId });
 };
 
 export const createContactService = async (data) => {
-  return await Contact.create(data);
+  return await ContactsCollection.create(data);
 };
 
 export const updateContactService = async (contactId, userId, data) => {
-  return await Contact.findOneAndUpdate({ _id: contactId, userId }, data, { new: true });
+  return await ContactsCollection.findOneAndUpdate({ _id: contactId, userId }, data, { new: true });
 };
 
 export const deleteContactService = async (contactId, userId) => {
-  return await Contact.findOneAndDelete({ _id: contactId, userId });
+  return await ContactsCollection.findOneAndDelete({ _id: contactId, userId });
 };
