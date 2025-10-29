78212778410dc6c40266dfdca3ea92ff379f125f
Artem Kulish <akulish247@gmail.com>
2025-10-29 11:59:15 +0100

    Fix

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,5 +1,5 @@
 import { Contact } from "../models/contact.js";
-import { uploadToCloudinary } from "../utils/uploadToCloudinary.js";
+import { uploadToCloudinary } from "../utils/cloudinary.js";
 
 export const createContact = async (req, res, next) => {
   try {
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,16 +1,32 @@
 import { v2 as cloudinary } from "cloudinary";
+import fs from "fs/promises";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+// 🔧 Конфігурація Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
+/**
+ * 
+ * @param {string} filePath 
+ * @returns {Promise<string>} 
+ */
 export const uploadToCloudinary = async (filePath) => {
-  return await cloudinary.uploader.upload(filePath, {
-    folder: "contacts_photos",
-  });
+  try {
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      folder: "contacts_photos",
+    });
+
+    await fs.unlink(filePath);
+
+    return uploadResult.secure_url;
+  } catch (error) {
+    console.error("Cloudinary upload error:", error);
+    throw new Error("Не вдалося завантажити зображення на Cloudinary");
+  }
 };
