c76a71f8f88fec977f08cd3e98df0312ab446bef
Artem Kulish <akulish247@gmail.com>
2025-11-01 16:37:52 +0100

    update

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,5 +1,8 @@
 export const calculatePaginationData = (count, perPage, page) => {
   const totalPages = Math.ceil(count / perPage);
+  //   const hasNextPage = Boolean(totalPages - page);
+  //   const hasPreviousPage = page !== 1;
+
   return {
     page,
     perPage,
--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -1,4 +1,6 @@
+
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export function getEnvVar(name, defaultValue) {
